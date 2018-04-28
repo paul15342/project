c9ba9367a687cf296e650aca2cd53a62741aefcd
paul <1003023075@qq.com>
2018-04-28 18:37:26 +0800

    update

---
+++ form.py
@@ -0,0 +1,11 @@
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
+
+class DataForm(Form):
+    address = StringField("接口地址",validators=[Required()])
+    agent_id = StringField("方案商ID",validators=[Required()])
+    agent_key = StringField("方案商秘钥",validators=[Required()])
+    user_name = StringField("测试账号",validators=[Required()])
+    order_number = StringField("外部6-30位数字充值订单号",validators=[Required()])
+    submit = SubmitField("submit")
